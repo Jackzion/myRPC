de4df41d3991a5b7254c4996c0d4e2f9b86760b8
ziio. <1934255852@qq.com>
2024-07-03 17:50:04 +0800

    使用动态代理，对consumer根据类信息进行统一请求

--- RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/EasyConsumerExample.java
+++ RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/EasyConsumerExample.java
@@ -5,20 +5,12 @@ import com.ziio.example.common.service.UserService;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
-//        UserService userService = null;
-//        User user = new User();
-//        user.setName("ziio");
-//        // 调用
-//        User newUser = userService.getUser(user);
-//        if(newUser!=null){
-//            System.out.println(newUser.getName());
-//        }else{
-//            System.out.println("User == null");
-//        }
         // 静态代理发送请求 (每个service一个代理，麻烦)
-        UserServiceProxy userServiceProxy = new UserServiceProxy();
+//        ServiceProxy serviceProxy = new ServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("ziio");
-        System.out.println(userServiceProxy.getUser(user).getName());
+        System.out.println(userService.getUser(user).getName());
     }
 }
--- RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/ServiceProxy.java
+++ RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/ServiceProxy.java
@@ -3,37 +3,41 @@ package com.ziio.example.consumer;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.ziio.example.common.model.User;
-import com.ziio.example.common.service.UserService;
 import com.ziio.example.consumer.serializer.JdkSerializer;
 import com.ziio.example.consumer.serializer.Serializer;
 import com.ziio.example.model.RpcRequest;
 import com.ziio.example.model.RpcResponse;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 /**
- * 静态代理
+ * 动态代理 , InvocationHandler实现
  */
-public class UserServiceProxy implements UserService {
+public class ServiceProxy implements InvocationHandler {
 
+    // proxy 动态代理每个方法都会经过 invoke , 即userService 每个接口都会得到增强实现
     @Override
-    public User getUser(User user) {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // 序列化器
         Serializer serializer = new JdkSerializer();
 
         //发请求
         RpcRequest rpcRequest = new RpcRequest();
-        rpcRequest.setServiceName(UserService.class.getName());
-        rpcRequest.setMethodName("getUser");
-        rpcRequest.setParameterTypes(new Class[]{User.class});
-        rpcRequest.setArgs(new Object[]{user});
+        rpcRequest.setServiceName(method.getDeclaringClass().getName());
+        rpcRequest.setMethodName(method.getName());
+        rpcRequest.setParameterTypes(method.getParameterTypes());
+        rpcRequest.setArgs(args);
 
         try {
+            // 請求序列化
             byte[] bodyBytes = serializer.serialize(rpcRequest);
             byte[] result;
+            // 发送 http' 请求
             try(HttpResponse httpResponse = HttpRequest.post("http://localhost:8881")
-                        .body(bodyBytes)
-                        .execute())
+                    .body(bodyBytes)
+                    .execute())
             {
                 result = httpResponse.bodyBytes();
             }
@@ -43,6 +47,5 @@ public class UserServiceProxy implements UserService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
