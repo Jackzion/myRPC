9e5649face337b5ac78ef7dad46af8663bce332c
ziio. <1934255852@qq.com>
2024-07-04 12:43:24 +0800

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### 什么是 RPC？
+
+定义：RPC（Remote Procedure Call）即远程过程调用，是一种计算机通信协议，它允许程序在不同的计算机之间进行通信和交互，就像本地调用一样。
+
+服务消费方只需要实现同样的 common 接口 ，无需关心服务如何实现，就能调用远程的服务，得到服务结果。
+
+参考框架：[Apache Dubbo](https://cn.dubbo.apache.org/zh-cn/)
+
+## 架构设计
+
+
--- RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/EasyConsumerExample.java
+++ RPC-demo/example-consumer/src/main/java/com/ziio/example/consumer/EasyConsumerExample.java
@@ -10,11 +10,11 @@ public class EasyConsumerExample {
         // 静态代理发送请求 (每个service一个代理，麻烦)
 //        ServiceProxy serviceProxy = new ServiceProxy();
 
-//        // 动态代理
-//        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
-//        User user = new User();
-//        user.setName("ziio");
-//        System.out.println(userService.getUser(user).getName());
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("ziio");
+        System.out.println(userService.getUser(user).getName());
 
         // 配置文件读取 test
         RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
